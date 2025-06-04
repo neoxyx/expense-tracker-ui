3ad503f3b97e3986cec6244c65750bf331b55a4b
Jhon <jhon.valdes@softgic.co>
2025-06-04 09:59:34 -0500

    ssr off

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://miappdevexpress-g0a2amexb9a3bub7.brazilsouth-01.azurewebsites.net/api'
+  apiUrl: 'https://expensetracker-api-bygph4e6g6e8eucq.canadacentral-01.azurewebsites.net/api'
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 // environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5025/api',
+  apiUrl: 'https://expensetracker-api-bygph4e6g6e8eucq.canadacentral-01.azurewebsites.net/api',
 };
